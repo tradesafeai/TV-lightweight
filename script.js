60112c633e1b9038b6d5d4f3f75e25ccc386dd67
tradesafeai <tradesafeai.io@gmail.com>
2025-07-13 13:46:41 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+console.log(typeof LightweightCharts); // Debug: Should log "object"
+const chart = LightweightCharts.createChart(document.getElementById('chart-container'), {
+  width: 400,
+  height: 300,
+  layout: { background: { type: 'solid', color: 'white' } }
+});
+const lineSeries = chart.addLineSeries();
+lineSeries.setData([
+  { time: '2019-04-11', value: 80.01 },
+  { time: '2019-04-12', value: 96.63 },
+  { time: '2019-04-13', value: 76.64 },
+  { time: '2019-04-14', value: 81.89 },
+  { time: '2019-04-15', value: 74.43 },
+  { time: '2019-04-16', value: 80.01 },
+  { time: '2019-04-17', value: 96.63 },
+  { time: '2019-04-18', value: 76.64 },
+  { time: '2019-04-19', value: 81.89 },
+  { time: '2019-04-20', value: 74.43 },
+]);
+chart.timeScale().fitContent();
